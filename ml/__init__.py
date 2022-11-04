77eba887ebf22db7348508c06852d5a0acb521c3
Paulina <paulinamacurek1@gmail.com>
2022-11-04 22:51:11 +0100

    Change time.sleep + change model_jezyka.arpa

--- ml/__init__.py
+++ ml/__init__.py
@@ -13,19 +13,17 @@ def speech_recognition(path):
     data = read(path)
     data_after_squeezing = data.data.squeeze().astype('float32')
     fs = data.rate
-   # feats = processor(data_after_squeezing, sampling_rate=fs, return_tensors='pt', padding=True)  # .to('cuda')
-   # out = model(input_values=feats.input_values)
-   # predicted_ids = torch.argmax(out.logits, dim=-1)
-  #  sent = processor.batch_decode(predicted_ids)[0]
-
-
-
-
+    '''
+    feats = processor(data_after_squeezing, sampling_rate=fs, return_tensors='pt', padding=True)  # .to('cuda')
+    out = model(input_values=feats.input_values)
+    predicted_ids = torch.argmax(out.logits, dim=-1)
+    sent = processor.batch_decode(predicted_ids)[0]
+    print(sent)
+    '''
     feats = processor(data_after_squeezing, sampling_rate=fs, return_tensors='pt', padding=True)  # .to('cuda')
     out = model(input_values=feats.input_values)
     sent1 = decoder.decode(out.logits.cpu().detach().numpy()[0])
     print('You said: ', sent1)
-
 
     return sent1
 
--- voice_to_file/__init__.py
+++ voice_to_file/__init__.py
@@ -8,7 +8,7 @@ def get_voice_file():
     recording = sd.rec(int(DURATION * FREQUENCY), samplerate=FREQUENCY, channels=CHANNELS)
     print('RECORDING',end='')
     path = PATH_LIVE_RECORD + get_date() + RECORDING_FORMAT
-    for i in range(60):
+    for i in range(55):
         time.sleep(0.1)
         print('.', end='')
 #    sd.wait()
